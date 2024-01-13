c19101febd2218ccdf9c5231b1c8ed0f71abeead
Myat Thiri Khin <137380130+myatthiri98@users.noreply.github.com>
2024-01-13 22:45:04 +0630

    style: UI style && color
    style: UI style

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import StartGameScreen from './screens/StartGameScreen'
+import {ImageBackground, StatusBar, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import StartGameScreen from './screens/StartGameScreen';
+import LinearGradient from 'react-native-linear-gradient';
 
 const App = () => {
   return (
-   <StartGameScreen/>
-  )
-}
+    <LinearGradient colors={['#3498db', '#2c3e50']} style={styles.rootScreen}>
+      <StatusBar backgroundColor="#3498db" barStyle="light-content" />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const pressHandler = () => {
+  console.log('Pressed!');
+};
 
 const PrimaryButton = ({children}) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({pressed}) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{color: '#042533'}}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default PrimaryButton
+export default PrimaryButton;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#399CC7',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
